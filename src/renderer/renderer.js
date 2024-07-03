59fd1802bd55205838eaa93979714fb6791e1ec0
Jules Grateau <jules.grateau@gmail.com>
2024-07-03 14:56:43 +0900

    Moved user config file

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
-const config = require('../config.json');
+const config = require('../config/user_config.json');
 
 window.addEventListener('load', function () {
   document.getElementById('message').addEventListener('keyup', (e) => {
@@ -21,14 +21,14 @@ const generateMessage = async (message, fileName) => {
   const request = {
     input: {text: message},
     voice: {
-      ...config.voice,
+      ...config.VOICE,
     },
     audioConfig: {audioEncoding: 'MP3'},
   };
 
   // Performs the text-to-speech request
   axios.post(
-    `https://texttospeech.googleapis.com/v1/text:synthesize?key=${config.api_key}`,
+    `https://texttospeech.googleapis.com/v1/text:synthesize?key=${config.API_KEY}`,
     request
   ).then((response) => {
   const audioContent = response.data.audioContent;
